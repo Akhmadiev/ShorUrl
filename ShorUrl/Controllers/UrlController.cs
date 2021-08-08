b930ce031e2d47a0aadf5c85af93727a5ddc3a94
Марат Ахмадиев <m.akhmadiev@bars.group>
2021-08-08 18:07:52 +0300

    Add project files.

---
+++ ShorUrl/Controllers/UrlController.cs
@@ -0,0 +1,26 @@
+﻿using HashidsNet;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShorUrl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetShort(string url)
+        {
+            var hashids = new Hashids(url); 
+            var id = hashids.Encode(1, 2, 3);
+
+            return id;
+        }
+    }
+}
